b2540f7c40478a010d55a4ae7cac0dbc823f77d2
Pedro Burgos <pedrobmp3@gmail.com>
2018-05-12 16:27:06 -0300

    Adicionada busca básica

--- src/pages/searchTest/searchTest.ts
+++ src/pages/searchTest/searchTest.ts
@@ -7,8 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController) {
+  searchQuery: string = '';
+  items: string[];
 
+  constructor() {
+    this.initializeItems();
   }
 
+  initializeItems() {
+    this.items = [
+      'Abrigo A',
+      'Abrigo B',
+      'Abrigo C'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
