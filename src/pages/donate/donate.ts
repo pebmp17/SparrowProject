e350f330eaa3717904475e81b50625922a179083
Pedro Burgos <pedrobmp3@gmail.com>
2018-05-21 04:27:47 -0300

    Trocando searchTest por donate e fazendo estrutura do banner

---
+++ src/pages/donate/donate.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-donate',
+  templateUrl: 'donate.html'
+})
+export class DonatePage {
+
+  constructor() {
+  }
+}
