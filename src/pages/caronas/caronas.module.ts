756a4d8ae795063c74384ebcefbbb3badd42603d
Pedro Burgos <pedrobmp3@gmail.com>
2018-05-21 03:06:56 -0300

    Página de Doação quase feita e iniciando home

---
+++ src/pages/caronas/caronas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CaronasPage } from './caronas';
+
+@NgModule({
+  declarations: [
+    CaronasPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CaronasPage),
+  ],
+})
+export class CaronasPageModule {}
