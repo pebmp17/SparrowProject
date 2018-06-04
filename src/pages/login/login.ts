0304605018b7c5163e8b3d893d0139ce9534fa06
Pedro Burgos <pedrobmp3@gmail.com>
2018-06-04 02:25:34 -0300

    Págins mapas, cartilha e doação

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { NavController } from 'ionic-angular';
 
+import { UserData } from '../../providers/user-data';
+
 import { UserOptions } from '../../interfaces/user-options';
+
+import { TabsPage } from '../tabs-page/tabs-page';
 
 @Component({
   selector: 'page-user',
@@ -10,10 +15,29 @@ import { UserOptions } from '../../interfaces/user-options';
 })
 export class LoginPage {
   login: UserOptions = { username: '', password: '' };
+  signup: UserOptions = { username: '', password: '' };
+  
   segment = "loginView"
-  constructor(public nav: NavController) { }
+  submitted = false;
 
+  constructor(public nav: NavController,  public userData: UserData) { }
+  onLogin(form: NgForm) {
+    this.submitted = true;
 
+    if (form.valid) {
+      this.userData.login(this.login.username);
+      this.nav.push(TabsPage);
+    }
+  }
+
+  onSignup(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.userData.signup(this.signup.username);
+      this.nav.push(TabsPage);
+    }
+  }
   backToRoot() {
     this.nav.setRoot('TabsPage')
   }
